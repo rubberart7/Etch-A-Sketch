3ace803cad19d00f3bd3945680976c9dfa7a4367
mbyaf <rubberart7@users.noreply.github.com>
2025-04-24 16:43:09 -0400

    added some comments to explain js code and css styling

--- script.js
+++ script.js
@@ -6,23 +6,28 @@ const defaultSquares = 16;
 const sketchArea = document.getElementById("sketch-area");
 
 function setGridSize(squares) {
+    sketchArea.style.width = `${defaultSize}px`;
+    sketchArea.style.height = `${defaultSize}px`;
     for (let i = 0; i < (squares * squares); i++) {
-        sketchArea.style.width = `${defaultSize}px`;
-        sketchArea.style.height = `${defaultSize}px`;
         const gridCell = document.createElement("div");
         gridCell.style.width = `${(defaultSize / squares)}px`;
         gridCell.style.height = `${(defaultSize / squares)}px`;
-        //try to figure out this top part later for why
+        // Sets each cell’s width and height so they fit evenly inside the sketch area.
         gridCell.classList.add("cell");
     
     
         gridCell.addEventListener("mouseover", function(){
             gridCell.style.backgroundColor = "darkblue";
         });
+        
+    
     
         sketchArea.appendChild(gridCell);
         console.log(`Added cell number ${i}`);
     }
+
+    // loop causes it to add an event listener to each grid to turn blue
+    // in the future look into using event delegation instead
 }
 
 function removeGridCells() {
@@ -46,6 +51,8 @@ resetButton.addEventListener("click", function(){
     removeGridCells();
     setGridSize(defaultSquares);
 });
+
+// each time you reset you remove all the cells and create new ones
 
 
 
